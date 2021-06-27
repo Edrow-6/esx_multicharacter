685e7d721d984942e12aaae495b91105d1be191d
Cédric Dnst <cedric.dnst@gmail.com>
2021-06-27 20:28:22 +0200

    Add French language file

---
+++ locales/fr.lua
@@ -0,0 +1,11 @@
+Locales['fr'] = {
+	['male'] = "Homme",
+	['female'] = "Femme",
+	['delete_label'] = "Supprimer %s %s ?",
+	['select_char'] = "Sélectionner un personnage",
+	['create_char'] = "Créer un nouveau personnage",
+	['char_play'] = "Jouer ce personnage",
+	['char_delete'] = "Supprimer ce personnage",
+	['cancel'] = "Annuler",
+	['confirm'] = "Confirmer",
+}
